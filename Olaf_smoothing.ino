656af94ca1f1074cd9b51bc3fda8d5b90a4e79de
csnap11 <76184955+csnap11@users.noreply.github.com>
2026-01-28 00:14:51 -0500

    Add files via upload

--- Olaf_smoothing.ino
+++ Olaf_smoothing.ino
@@ -7,13 +7,15 @@
 
 #define RC_Signal_PIN 11//PIN that will read PWM signal from the RC receiver
 #define Servo_signal_PIN 5//PIN that will send PWM signal to the servo
-int channel;
-int pos_current; //current pos
-int pos_target; //desired pos
+int channel; //RC signal Channel
+int pos_current; //current position, x0
+int pos_target; //desired position, x1
 int delta_pos;//difference between x0 and x1
-int pos_max;
-int pos_min;
-int vel; //delta div by ticks
+int pos_max; //Max of Servo usually 180
+int pos_min;// Min of Servo usually 90
+int vel; //velocity, delta divided by ticks
+int vel0;
+int accel;
 int ticks; // delay
 Servo myservo;
 
@@ -29,24 +31,29 @@ void setup() {
   ticks = 10;
   pos_max = 180;
   pos_min = 0;
+  vel0 = 0;
 }
 
 void loop() {
   // Controller Read and map
-  channel = pulseIn(RC_Signal_PIN, HIGH);
+  channel = pulseIn(RC_Signal_PIN, HIGH); //converts RC signal in pulse that can be read
   //Serial.println(channel);
-  pos_target = limit(map(channel, 985, 1970, 0, 180), pos_min, pos_max);
+  pos_target = limit(map(channel, 985, 1970, 0, 180), pos_min, pos_max); //Maps the incoming Pulse and maps it to standard servo range 0-180
 
   //Velocity section
   delta_pos = pos_target - pos_current;
   Serial.println(delta_pos);
   vel = delta_pos / ticks;
-  Serial.println(vel);
+  Serial.println(vel);\
 
+  //Acceleration section
+  accel = ((vel - vel0) / ticks);
+  vel0 = vel0 + accel;
+  //Other ideas
+  //
 
-  
   //Update and write
-  pos_current = pos_current + vel;
+  pos_current = pos_current + vel0;
   Serial.println(pos_current);
   pos_current = limit(pos_current, pos_min, pos_max);
   Serial.println(pos_current);
@@ -54,11 +61,11 @@ void loop() {
   delay(25);
 }
 
-int limit( int val, int minVal, int maxVal) {
-   if (val < minVal) {
+int limit( int val, int minVal, int maxVal) {   //used to ensure signal sent to servo is always within range
+   if (val < minVal) {                          //if current position is less than Minimum value set position to 0
       val = minVal;
    }
-   if (val > maxVal) {
+   if (val > maxVal) {                          //if current position is greater than Maximum value set position to 180
       val = maxVal;
    }
    return val;
